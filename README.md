f9eaf4b1b4bf69cf2510c36f63f6bc33320a7134
John Garrison <splendidonkey@gmail.com>
2023-08-11 15:03:15 -0500

    added video walkthrough link

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ecommerce_backend
 
 ## Description
-The backend code for an ecommerce website
+The backend code for an ecommerce website.
+Video walkthrough available [here](https://drive.google.com/file/d/17cwJNGqn-MiaY3Twg_-IkIrAGUFBdVsQ/view).
 
 ## Installation
 ```
--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-// console.log(process.env.DB_NAME);
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASSWORD);
+console.log(`process.env.DB_NAME: ${process.env.DB_NAME}`);
+console.log(`process.env.DB_USER: ${process.env.DB_USER}`)
+console.log(`process.env.DB_PASSWORD: ${process.env.DB_PASSWORD}`);
 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(/*process.env.DB_NAME=*/'ecommerce_db', /*process.env.DB_USER=*/'root', /*process.env.DB_PASSWORD=*/null, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
